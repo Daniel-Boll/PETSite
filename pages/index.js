534cc90f626028a0b113ec0e3cb06a51d0b73e8a
Daniel Boll <danielboll.academico@gmail.com>
2021-05-20 18:27:55 -0300

    change: Added cards functionality
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 import useSWR from "swr";
 
@@ -10,10 +11,51 @@ const Home = () => {
   const locale = router.locale;
 
   const { data, error } = useSWR(`/locales/${locale}.json`, fetcher);
-
   return (
     <div className={styles.container}>
-      <h1>Hey! I'm a home - {data?.language}</h1>
+      <div className={styles.navbar}>
+        {data?.language == "pt" ? (
+          <p>
+            <strong>pt</strong> | en
+          </p>
+        ) : (
+          <p>
+            pt | <strong>en</strong>
+          </p>
+        )}
+      </div>
+      <div className={styles.mainCard}>
+        <Image
+          src="/images/pet_logo_complete.png"
+          alt="What is PETComp?"
+          width={235}
+          height={54}
+          className={styles.petLogo}
+        />
+        <p className={styles.whatIs}>{data?.home.whatIs}</p>
+      </div>
+      <hr className={styles.divisor} />
+      <div className={styles.whoWeAre}>
+        {data?.home.secondaryText
+          .split("?")
+          .slice(0, 2)
+          .map((question) => (
+            <p>{question}?</p>
+          ))}
+      </div>
+      {data?.home.cardsPa.map(([name, image]) => (
+        <div className={styles.secondaryCard}>
+          <Image
+            src={image}
+            alt="What is PETComp?"
+            width={125}
+            height={90}
+            className={styles.cardsImage}
+          />
+          <h2>{name}</h2>
+        </div>
+      ))}
+      {/* Footer */}
     </div>
   );
 };
